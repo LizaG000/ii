7ff64109215ca55585cc269767f7326748e6a91f
LizaG000 <elizaveta.vladimirovna.g@gmail.com>
2025-08-29 00:39:14 +0300

    лаба 3 задача 5

--- tasks/rock_paper_scissors.py
+++ tasks/rock_paper_scissors.py
@@ -1,14 +1,16 @@
 from random import randint
-t = ["Камень", "Бумага", "Ножницы"]
+t = ["Камень", "Бумага", "Ножницы", "Колодец"]
 computer = t[randint(0, 2)]
 player = False
 while player == False:
-    player = input("Камень, Ножницы, Бумага? > ")
+    player = input("Камень, Ножницы, Бумага, Колодец? > ")
     if player == computer:
         print("Ничья!")
     elif player == "Камень":
         if computer == "Бумага":
             print("Ты проиграл!", computer, "накрывает", player)
+        elif computer == "Колодец":
+            print("Ты проиграл!", player, "тонeт в", computer)
         else:
             print("Ты выиграл!", player, "разбивает", computer)
     elif player == "Бумага":
@@ -19,8 +21,17 @@ while player == False:
     elif player == "Ножницы":
         if computer == "Камень":
             print("Ты проиграл!", computer, "разбивает", player)
+        elif computer == "Колодец":
+            print("Ты проиграл!", player, "тонут в", computer)
         else:
             print("Ты выиграл!", player, "режет", computer)
+    elif player == "Колодец":
+        if computer == "Бумага":
+            print("Ты проиграл!", computer, "накрывает", player)
+        elif computer == "Камень":
+            print("Ты выиграл!", computer, "тонeт в", player)
+        else:
+            print("Ты выиграл!", computer, "тонут в", player)
     else:
         print("Некорректный ход!")
     player = False
